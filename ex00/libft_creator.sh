5f4aea7551a53985dc546104c703ef06c3a3cd70
shelton Adams <sadams@c4r9s3.wethinkcode.co.za>
2020-06-26 16:23:43 +0200

    exercise 00

--- ex00/libft_creator.sh
+++ ex00/libft_creator.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ar rc libft.a ft_putchar.o ft_putstr.o ft_strlen.o ft_swap.o
+gcc -c ft_putchar.c ft_putstr.c ft_strlen.c ft_swap.c || ar rc libft.a ft_putchar.o ft_putstr.o ft_strlen.o ft_swap.o
